f4efb7397de0ecf24c4fe498edb9287a95b02c72
SukhanovIgorG <sukhanovgarik@yandex.ru>
2026-02-14 14:50:06 +0300

    feat: hide keyboard on click

--- components/forms/register-form/index.tsx
+++ components/forms/register-form/index.tsx
@@ -3,7 +3,13 @@ import { SPACING } from "@/tokens";
 import { normalizePhone } from "@/utils";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Controller, useForm } from "react-hook-form";
-import { Alert, StyleSheet, View } from "react-native";
+import {
+  Alert,
+  Keyboard,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
 
 import { Divider } from "../../ui";
 import { registerDefaultValues, registerSchema } from "./constants";
@@ -32,96 +38,98 @@ export const RegisterForm = () => {
   };
 
   return (
-    <View style={styles.wrapper}>
-      <View style={styles.formContainer}>
-        <Divider />
-        <Controller
-          name="fio"
-          control={control}
-          render={({ field: { onChange, onBlur, value } }) => (
-            <FormInput
-              error={errors.fio?.message}
-              onBlur={onBlur}
-              onChangeText={onChange}
-              value={value}
-              placeholder="ФИО"
-              required
-            />
-          )}
-        />
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+      <View style={styles.wrapper}>
+        <View style={styles.formContainer}>
+          <Divider />
+          <Controller
+            name="fio"
+            control={control}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <FormInput
+                error={errors.fio?.message}
+                onBlur={onBlur}
+                onChangeText={onChange}
+                value={value}
+                placeholder="ФИО"
+                required
+              />
+            )}
+          />
 
-        <Controller
-          name="email"
-          control={control}
-          render={({ field: { onChange, onBlur, value } }) => (
-            <FormInput
-              error={errors.email?.message}
-              onBlur={onBlur}
-              onChangeText={onChange}
-              value={value}
-              placeholder="email"
-              type="email"
-              required
-            />
-          )}
-        />
+          <Controller
+            name="email"
+            control={control}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <FormInput
+                error={errors.email?.message}
+                onBlur={onBlur}
+                onChangeText={onChange}
+                value={value}
+                placeholder="email"
+                type="email"
+                required
+              />
+            )}
+          />
 
-        <Controller
-          name="phone"
-          control={control}
-          render={({ field: { onChange, onBlur, value } }) => (
-            <FormInput
-              error={errors.phone?.message}
-              onBlur={() => {
-                const normalized = normalizePhone(value);
-                onChange(normalized);
-                onBlur();
-              }}
-              onChangeText={onChange}
-              value={value}
-              placeholder="Телефон"
-              type="phone"
-              required
-            />
-          )}
-        />
+          <Controller
+            name="phone"
+            control={control}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <FormInput
+                error={errors.phone?.message}
+                onBlur={() => {
+                  const normalized = normalizePhone(value);
+                  onChange(normalized);
+                  onBlur();
+                }}
+                onChangeText={onChange}
+                value={value}
+                placeholder="Телефон"
+                type="phone"
+                required
+              />
+            )}
+          />
 
-        <Controller
-          name="gender"
-          control={control}
-          render={({ field: { onChange, onBlur, value } }) => (
-            <FormSelect
-              error={errors.gender?.message}
-              onBlur={onBlur}
-              onChangeText={onChange}
-              value={value}
-              placeholder="Пол"
-              required
-              options={[
-                { label: "Мужской", value: "male" },
-                { label: "Женский", value: "female" },
-              ]}
-            />
-          )}
-        />
+          <Controller
+            name="gender"
+            control={control}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <FormSelect
+                error={errors.gender?.message}
+                onBlur={onBlur}
+                onChangeText={onChange}
+                value={value}
+                placeholder="Пол"
+                required
+                options={[
+                  { label: "Мужской", value: "male" },
+                  { label: "Женский", value: "female" },
+                ]}
+              />
+            )}
+          />
 
-        <Controller
-          name="agreement"
-          control={control}
-          render={({ field: { onChange, onBlur, value } }) => (
-            <FormSwitch
-              label="Согласие с условиями"
-              onValueChange={onChange}
-              onBlur={onBlur}
-              value={value}
-              error={errors.agreement?.message}
-            />
-          )}
-        />
+          <Controller
+            name="agreement"
+            control={control}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <FormSwitch
+                label="Согласие с условиями"
+                onValueChange={onChange}
+                onBlur={onBlur}
+                value={value}
+                error={errors.agreement?.message}
+              />
+            )}
+          />
+        </View>
+
+        <Button title="Сохранить" onPress={handleSubmit(onSubmit)} />
       </View>
-
-      <Button title="Сохранить" onPress={handleSubmit(onSubmit)} />
-    </View>
+    </TouchableWithoutFeedback>
   );
 };
 
