ca14e5484b6fb3ddfd3258bb399f06399ca50f48
SukhanovIgorG <sukhanovgarik@yandex.ru>
2026-02-14 15:03:24 +0300

    hotfix

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -22,10 +22,6 @@ export default function RootLayout() {
         >
           <Stack initialRouteName="index">
             <Stack.Screen name="index" options={{ headerShown: false }} />
-            <Stack.Screen
-              name="modal"
-              options={{ presentation: "modal", title: "Modal" }}
-            />
           </Stack>
           <StatusBar style="auto" />
         </ThemeProvider>
